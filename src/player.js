2e85c2d5150f845e1f3daf6ebdefca158260c97c
Ben Gilbert <bensgilbert@outlook.com>
2022-01-14 10:37:39 +0000

    player tilt effect

--- src/player.js
+++ src/player.js
@@ -5,19 +5,24 @@ class Player {
     constructor(sprite) {
         this.position = { x: width / 2, y: height - 80 };
         this.sprite = sprite;
+        this.tilt = false;
     }
 
     update() {
+        this.tilt = false;
+
         if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // keycode 65 = "a"
             // prevent player crossing left side of screen
             if (this.position.x - playerSize / 2 > playerSpeed) {
                 this.position.x -= playerSpeed;
+                this.tilt = true;
             }
         }
         if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { // keycode 68 = "d"
             // prevent player crossing right side of screen
             if (this.position.x + playerSize / 2 < width - playerSpeed) {
                 this.position.x += playerSpeed;
+                this.tilt = true;
             }
         }
     }
@@ -26,7 +31,7 @@ class Player {
         push();
 
         imageMode(CENTER);
-        image(this.sprite, this.position.x, this.position.y, playerSize, playerSize);
+        image(this.sprite, this.position.x, this.position.y, playerSize - (this.tilt?playerSize/4:0), playerSize);
 
         pop();
     }
