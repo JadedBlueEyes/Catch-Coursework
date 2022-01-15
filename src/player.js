f17c04007f563daa2732bb84320b7950822db1d4
Joel Ellis <joel@ellis.link>
2022-01-15 11:27:34 +0000

    Calculate player coordiantes from top left

--- src/player.js
+++ src/player.js
@@ -3,7 +3,9 @@ const playerSpeed = 5;
 
 class Player {
     constructor(sprite) {
-        this.position = { x: width / 2, y: height - 80 };
+        this.position = {
+            x: ( width / 2 ) - (playerSize / 2),
+            y: ( height - 80 ) - (playerSize / 2)};
         this.sprite = sprite;
         this.tilt = false;
     }
@@ -13,14 +15,14 @@ class Player {
 
         if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) { // keycode 65 = "a"
             // prevent player crossing left side of screen
-            if (this.position.x - playerSize / 2 > playerSpeed) {
+            if (this.position.x  > playerSpeed) {
                 this.position.x -= playerSpeed;
                 this.tilt = true;
             }
         }
         if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) { // keycode 68 = "d"
             // prevent player crossing right side of screen
-            if (this.position.x + playerSize / 2 < width - playerSpeed) {
+            if (this.position.x + playerSize < width - playerSpeed) {
                 this.position.x += playerSpeed;
                 this.tilt = true;
             }
@@ -30,7 +32,6 @@ class Player {
     draw() {
         push();
 
-        imageMode(CENTER);
         image(this.sprite, this.position.x, this.position.y, playerSize - (this.tilt ? playerSize / 4 : 0), playerSize);
 
         pop();
